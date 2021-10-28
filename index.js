ccd16125397fbb0b49c41202b1a97828e7640d9f
Austin Merrick <austin.merrick@unlv.edu>
2021-10-28 14:01:10 -0700

    Adding RESTful API

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const client = new Client({intents: [
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_PRESENCES,
     Intents.FLAGS.GUILD_MEMBERS,
-] });
+]});
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
+client.once('ready', async () => {
 	console.log('Ready!');
+    await getMessages();
+    startAPI();
 });
 
 client.on("messageCreate", async (message) => {
@@ -21,3 +23,25 @@ client.on("messageCreate", async (message) => {
 })
 
 client.login(token);
+
+async function getMessages() {
+    const channel = client.channels.cache.get("903383096446038026");
+    let messages = await channel.messages.fetch({ limit: 100 })
+    console.log(`Received ${messages.size} messages`);
+    return messages
+}
+
+function startAPI() {
+    const express = require('express')
+    const app = express()
+    const port = process.env.PORT || 3000
+
+    app.get('/', async function (req, res) {
+        let messages = await getMessages()
+        res.json(messages)
+    })
+       
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+}
